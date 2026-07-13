11a120d9de8d08ce45153be7ce5cf753b194d741
WWILISS-STUDIO <wwiliss.studio@gmail.com>
2026-07-13 19:50:39 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 // Replace the # values below with your real profile or song links.
 window.WWILISS_LINKS = {
-  spotify: "#",
-  youtube: "#",
-  youtubeMusic: "#",
-  appleMusic: "#",
-  tiktok: "#",
-  facebook: "#",
-  instagram: "#",
+  spotify: "https://open.spotify.com/artist/7lAKDi9vIqURSiohT2pVTP?si=v9OsETORTCqM4yuYOo4mWw",
+  youtube: "https://youtube.com/@wwiliss.studio?si=NrNX3BD-FVOibwnE",
+  youtubeMusic: "https://music.youtube.com/@wwiliss.studio?si=JkYMMq4x0oHVJU3q",
+  appleMusic: "https://music.apple.com/us/artist/wwiliss-studio/1896510188",
+  tiktok: "https://www.tiktok.com/@wwiliss.studio?is_from_webapp=1&sender_device=pc",
+  facebook: "https://www.facebook.com/wwiliss",
+  instagram: "https://www.instagram.com/wwiliss.studio",
   beacons: "https://beacons.ai/wwiliss.studio"
 };
